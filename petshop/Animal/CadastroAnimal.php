4472936d80b8a2119b0a6f4fa48e91e9e639ae1b
Willonrf <willonrf@gmail.com>
2021-01-08 15:14:08 -0300

    ProvaII

---
+++ petshop/Animal/CadastroAnimal.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cadastro de Animal</title>
+    </head>
+    <body>
+        <!--
+            Na raiz do projeto, deve existir uma página
+            com dois links, um para o cadastro de
+            espécie e outro para o cadastro de animal.
+        -->
+    </body>
+</html>
---
+++ petshop/Especie/CadastroEspecie.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cadastro de especie</title>
+    </head>
+    <body>
+        <!--
+            Na raiz do projeto, deve existir uma página
+            com dois links, um para o cadastro de
+            espécie e outro para o cadastro de animal.
+        -->
+    </body>
+</html>
